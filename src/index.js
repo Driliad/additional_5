73a4bfb4d4796d167df783b0b14ab6b3cb237c9e
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2018-03-04 02:55:06 +0300

    321

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  return str.lenght % 2 > 0 ? false : true
+  bracketsConfig.forEach((a, i) => {
+		while (str.includes(bracketsConfig[i][0] + bracketsConfig[i][1])) {
+			str = str.slice(0, str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]))
+			+ str.slice(str.indexOf(bracketsConfig[i][0] + bracketsConfig[i][1]) + 2);	
+		}		
+	})
+ 	return (str.length > 0) ? false : true;
 }
